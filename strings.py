4f8eaafff6c9e70ac2c90733989758195e630fcc
Dom <z5162871@student.unsw.edu.au>
2018-07-27 18:30:21 +1000

    did some dhit

--- strings.py
+++ strings.py
@@ -1,2 +1,14 @@
 strings = ['This', 'list', 'is', 'now', 'all', 'together']
-sentence = ''
+strings1 = ""
+
+i = 0
+
+for i in strings:
+    strings1 += i
+    if(i != strings[-1]):
+        strings1 += " "
+
+
+print(strings1)
+
+print(' '.join(strings))
